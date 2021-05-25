dc3539bfb9db3ae32fe4010019a267582b6ff6d7
malcolmbly <malcolmhbailey@gmail.com>
2021-05-25 17:15:00 -0600

    Added comments

--- src/books/Book.java
+++ src/books/Book.java
@@ -4,10 +4,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Pattern;
-
+/***
+ * {@code Book} stores the information for a book.
+ * 
+ * @param title: the title of a book
+ * @param author: the author of a book
+ * @param year: the year a book was written
+ * 
+ * @author Malcolm Bailey
+ * 
+ */
 public class Book implements Comparable<Book>{
 	
 	private String title;
@@ -31,15 +38,21 @@ public class Book implements Comparable<Book>{
 	public int getYear() {
 		return year;
 	}
-	
+	/***
+	 * Displays book object as a string.
+	 */
 	@Override
 	public String toString() {
 		return String.format(title + " by " + author + " (%d)", year);
 	}
 	
+	/***
+	 * Reads a list of books from a csv file.
+	 * @param filePath: the path of the csv file
+	 * @return returns an ArrayList containing each book read from the csv file at {@code filePath}
+	 * @throws FileNotFoundException if no csv file is found at the given {@code filePath}
+	 */
 	public static ArrayList<Book> getList(String filePath) throws FileNotFoundException{
-		//do I need to mention https://www.stackchief.com/blog/How%20to%20read%20a%20CSV%20file%20in%20Java%20%7C%20with%20examples
-		// since I learned about buffered reader from them.
 		
 		ArrayList<Book> books = new ArrayList<Book>();
 		String line;
@@ -48,16 +61,26 @@ public class Book implements Comparable<Book>{
 		try (Scanner sc = new Scanner(file)){
 			
 			while(sc.hasNextLine()) {
+				//while there is another line, load the next line.
 				line = sc.nextLine();
+				//generate the individual tokens based on commas that have no word boundaries on either side.
 				String [] tokens = line.split("\\b,\\b");
 				
 				if (tokens.length == 3){
+					//if there are 3 total tokens that means the format is most likely correct (title, author, year)
+					// this means we can store the various parts in a book object and add it to the ArrayList.
 					String bookTitle = tokens[0];
 					String bookAuthor = tokens[1];
-					Integer bookYear = Integer.parseInt(tokens[2]);
-					
-					Book book = new Book(bookTitle, bookAuthor, bookYear);
-					books.add(book);
+					try {
+						//Check that the third token is in fact an int, and add it to the list if is.
+						Integer bookYear = Integer.parseInt(tokens[2]);
+						Book book = new Book(bookTitle, bookAuthor, bookYear);
+						books.add(book);
+					} catch (NumberFormatException e) {
+						System.err.println("Failed to read the following line: " + line + "\n" 
+								+ "Please confirm that the third token is a number.");
+					}
+
 				} else {
 					System.err.println("Failed to read the following line: " + line + "\n" 
 							+ "Please confirm that the format is \"Title, Author, Year\".");
@@ -70,7 +93,12 @@ public class Book implements Comparable<Book>{
 			throw new FileNotFoundException("Could not find " + file);
 		}
 	}
-	
+	/***
+	 * Compares book object to another book {@code b} first on title then on author
+	 * @param b: a book object to be compared to.
+	 * @return negative int if the title of book {@code b} is alphabetically greater than the book, 0 if they're equal, and positive 
+	 * if book {@code b} is less than the book.
+	 */
 	public int compareTo(Book b) {
 
 		int compare = this.title.compareTo(b.title);
@@ -81,8 +109,20 @@ public class Book implements Comparable<Book>{
 		return compare;
 	}
 	
+	
+	/***
+	 * Implements the Comparator's compare method for book objects to sort in reverse alphabetical order.
+	 * @author malc
+	 *
+	 */
 	public static class ReverseAlphabeticalCompare implements Comparator<Book>{
 		
+		/**
+		 * Compares two books using reverse alphabetical order, first with title and then with author if two titles are the same.
+		 * @param a,b the books to be compared to each other.
+		 * @return negative int if the book {@code b} is alphabetically greater than book {@code a}, 0 if they're equal, 
+		 * and positive if book {@code b} is less than book {@code a}.
+		 */
 		public int compare(Book a, Book b) {
 			int compare = (a.title.compareTo(b.title)) * -1;
 			
--- src/books/BookApp.java
+++ src/books/BookApp.java
@@ -1,6 +1,5 @@
 package books;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -8,15 +7,24 @@ import java.util.Scanner;
 
 import books.Book.ReverseAlphabeticalCompare;
 
+/***
+ * Reads a list of books from a given CSV file path,
+ * then sorts the books based on their titles and authors
+ * @author Malcolm Bailey
+ *
+ */
 public class BookApp {
 
 	public static void main(String[] args) {
+		//Open a scanner to accept the file path of the csv file.
 		ArrayList<Book> books = new ArrayList<Book>();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter path to book csv file: ");
 		String filePath = sc.nextLine();
+		sc.close();
 		
 		try {
+			//Runs the getList method to add all the books listed in the CSV file into a list.
 			books = Book.getList(filePath);
 			System.out.println("Successfully added " + books.size() + " books to the list.");
 			
@@ -25,13 +33,17 @@ public class BookApp {
 			e.printStackTrace();
 		}
 		
+		// Sorts the book using both the natural order of the compareTo method
 		Collections.sort(books);
 		System.out.println("Books sorted by natural order:");
 		for (Book book : books) {
 			System.out.println(book);
 		}
-		System.out.println();
 		
+		for (int i = 0; i<3; i++) {
+			System.out.println();	
+		}
+		//Sorts the book using the reverse alphabetical order from a comparator.
 		ReverseAlphabeticalCompare reverseAlphabeticalCompare = new ReverseAlphabeticalCompare();
 		Collections.sort(books, reverseAlphabeticalCompare);
 		System.out.println("Books sorted by reverse alphabetical (natural) order:");
