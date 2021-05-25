a9ea0f33317ef6ca1bf8f900d488b2ef28c5a4d6
malcolmbly <malcolmhbailey@gmail.com>
2021-05-25 16:13:46 -0600

    Added several methods to book and created BookApp

--- src/books/Book.java
+++ src/books/Book.java
@@ -1,10 +1,82 @@
 package books;
 
-public class Book implements Comparable<Book>{
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+public class Book implements Comparable<Book>{
+	
+	private String title;
+	private String author;
+	private int year;
+
+	Book (String bookTitle, String bookAuthor, int bookYear){
+		title = bookTitle;
+		author = bookAuthor;
+		year = bookYear;		
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format(title + " by " + author + "(%d)", year);
+	}
+	
+	public static ArrayList<Book> getList(String filePath) throws FileNotFoundException{
+		//do I need to mention https://www.stackchief.com/blog/How%20to%20read%20a%20CSV%20file%20in%20Java%20%7C%20with%20examples
+		// since I learned about buffered reader from them.
+		
+		ArrayList<Book> books = new ArrayList<Book>();
+		String line;
+		File file = new File(filePath);
+		
+		try (Scanner sc = new Scanner(file)){
+			
+			while(sc.hasNextLine()) {
+				line = sc.nextLine();
+				String [] tokens = line.split("[A-Za-z0-9],[A-Za-z0-9]");
+				
+				if (tokens.length == 3){
+					String bookTitle = tokens[0];
+					String bookAuthor = tokens[1];
+					Integer bookYear = Integer.parseInt(tokens[2]);
+					
+					Book book = new Book(bookTitle, bookAuthor, bookYear);
+					books.add(book);
+				} else {
+					System.err.println("Failed to read the following line:" + line + "\n" 
+							+ "Please confirm that the format is \"Title, Author, Year\".");
+				}
+			}	
+			return books;
+			
+		} catch (Exception FileNotFoundException) {
+			
+			throw new FileNotFoundException("Could not find " + file);
+		}
+	}
 	
 	public int compareTo(Book b) {
 		//TODO: Fill in Method
-		return 0;
+		int compare = this.title.compareTo(b.title);
+		
+		if(compare == 0) {
+			return this.author.compareTo(b.author);
+		}
+		return compare;
 	}
 }
--- src/books/BookApp.java
+++ src/books/BookApp.java
@@ -1,9 +1,33 @@
 package books;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class BookApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<Book> books = new ArrayList<Book>();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter path to book csv file: ");
+		String filePath = sc.nextLine();
+		
+		try {
+			books = Book.getList(filePath);
+			System.out.println("Successfully added " + books.size() + " books to the list.");
+			
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
+		Collections.sort(books);
+		System.out.println("Books sorted by natural order:");
+		for (Book book : books) {
+			System.out.println(book);
+		}
 
 	}
 
